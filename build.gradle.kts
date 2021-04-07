ab815990542d8efe39b1f68b777596aff557e297
Sergey Voloshin <git@varme.pw>
2021-04-08 00:49:52 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.7.2"
+    java
+}
+
+group = "varme.pw"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2021.1"
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      v1.0.0: init<br>""")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "varrcan-material-theme"
+
